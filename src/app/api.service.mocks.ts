4830e06ba9244c1348c826a344325257a188a028
mattmorton <matthewmorton@protonmail.com>
2020-06-18 08:58:42 +1000

    add tests for pipeline

---
+++ src/app/api.service.mocks.ts
@@ -0,0 +1,14 @@
+import { Item } from './api.service';
+
+export const comment: Item = {
+  by: 'hn_check',
+  id: 23532240,
+  kids: [
+    23533963
+  ],
+  parent: 23531825,
+  text: '&quot;What I have yet to see but always secretly wanted, however, is a database that natively supports incremental updates to materialized views&quot;<p>SQL Server ala 10+ years ago enters the discussion.',
+  time: 1592251131,
+  type: 'comment',
+  display: true
+}
---
+++ src/app/comment/comment.component.spec.ts
@@ -0,0 +1,60 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { CommentComponent } from './comment.component';
+import { ApiService } from '../api.service';
+import { comment } from '../api.service.mocks';
+import { of } from 'rxjs';
+import { DebugElement } from '@angular/core';
+
+describe('CommentComponent', () => {
+  let component: CommentComponent;
+  let fixture: ComponentFixture<CommentComponent>;
+  let apiServiceSpy: jasmine.SpyObj<ApiService>;
+
+  beforeEach(async(() => {
+
+    const spy = jasmine.createSpyObj('ApiService', ['getMultipleItems']);
+
+    TestBed.configureTestingModule({
+      declarations: [ CommentComponent ],
+      providers: [
+        { provide: ApiService, useValue: spy }
+      ],
+      imports: [ HttpClientTestingModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommentComponent);
+    component = fixture.componentInstance;
+    apiServiceSpy = TestBed.get(ApiService);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should increment the comment nested depth value by 1 when called', () => {
+    component.depth = 0;
+    const incrementedDepth = component.incrementDepth();
+    expect(incrementedDepth).toEqual(1);
+  })
+
+  it('should not render anything if a comment is not provided', () => {
+    const commentTextDe: DebugElement = fixture.debugElement;
+    const commentTextEl: HTMLElement = commentTextDe.nativeElement;
+    const comment = commentTextEl.querySelector('comment-container');
+    expect(comment).toBeNull();
+  })
+
+  it('should render inner html', () => {
+    component.comment = comment;
+    const commentTextDe: DebugElement = fixture.debugElement;
+    const commentTextEl: HTMLElement = commentTextDe.nativeElement;
+    const commentText = commentTextEl.querySelector('#comment-text');
+    expect(commentText).toBeDefined();
+  })
+});
